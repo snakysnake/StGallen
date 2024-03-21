fbb1bb92a32d9643ade55beb94d7037ea2ebb713
snakysnake <its.copyy@gmail.com>
2024-03-21 20:54:53 +0100

    evacuation bug

--- SwissCheeseFrontend/components/Person.vue
+++ SwissCheeseFrontend/components/Person.vue
@@ -68,24 +68,44 @@ export default {
     watch: {
         async yPos(nPos, oldPos) {
             this.movementAmount = Math.ceil(Math.random() * 25);
+
+            if (this.person.evacuate) {
+                this.movementAmount * 2;
+            }
             if (!this.wait) {
                 if (this.yPos > this.height - 10 || this.yPos < 2) {
                     this.yPos = this.initYPos;
                 }
                 this.wait = true;
-                await this.sleep(Math.ceil(Math.random() * 500) * this.movementAmount);
+                let sleepTime = Math.ceil(Math.random() * 200) * this.movementAmount;
+                if (this.person.evacuate) {
+                    sleepTime / 40
+                }
+
+                await this.sleep(sleepTime);
+
                 this.move();
             }
             this.wait = false;
         },
         async xPos(nPos, oldPos) {
             this.movementAmount = Math.ceil(Math.random() * 25);
+
+            if (this.person.evacuate) {
+                this.movementAmount * 2;
+            }
             if (!this.wait) {
                 if (this.xPos > this.width - 10 || this.xPos < 2) {
                     this.xPos = this.initXPos;
                 }
                 this.wait = true;
-                await this.sleep(Math.floor(Math.random() * 500) * this.movementAmount);
+                let sleepTime = Math.floor(Math.random() * 200) * this.movementAmount;
+                if (this.person.evacuate) {
+                    sleepTime / 40
+                }
+
+                await this.sleep(sleepTime);
+
                 this.move();
             }
             this.wait = false;
@@ -104,18 +124,59 @@ export default {
     methods: {
         move() {
             // Function to simulate movement
-            const rand = Math.random();
-            if (rand <= 0.25) {
-                this.right();
-            }
-            else if (rand <= 0.5) {
-                this.left();
-            }
-            else if (rand <= 0.75) {
-                this.up();
+            let rand = Math.random();
+
+            if (this.person.evacuate) {
+                // invite person to move to closest edge...
+                const differences = {
+                    left: this.xPos,
+                    top: this.yPos,
+                    right: this.width - this.xPos,
+                    bottom: this.height - this.yPos
+                };
+
+                // Find the minimum difference
+                let minDifference = Infinity;
+                let minSide = "";
+
+                for (const [side, difference] of Object.entries(differences)) {
+                    if (difference < minDifference) {
+                        minDifference = difference;
+                        minSide = side;
+                    }
+                }
+
+                if (Math.random() < 90) {
+                    if (minSide === 'left') {
+                        this.left();
+                    }
+
+                    if (minSide === 'right') {
+                        this.right();
+                    }
+
+                    if (minSide === 'top') {
+                        this.up();
+                    }
+
+                    if (minSide === 'bottom') {
+                        this.down();
+                    }
+                }
             }
             else {
-                this.down();
+                if (rand <= 0.25) {
+                    this.right();
+                }
+                else if (rand <= 0.5) {
+                    this.left();
+                }
+                else if (rand <= 0.75) {
+                    this.up();
+                }
+                else {
+                    this.down();
+                }
             }
         },
         up() {
